07673daa191e80cd8f8be287ed086c336269ba54
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 21:00:05 +0530

    Create Hun.java

---
+++ Hun.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Hun
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        String n=in.next();
+        for(int i=0;i<n.length();i++)
+        {
+            System.out.print(n.charAt(i)+" ");
+        }
+}
+}
+
+
+
+
